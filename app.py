43126a8d01d6f26f85c8800ecee13207cfc8e359
porchlightKeeper <porchlightkeeper@gmail.com>
2024-03-17 13:12:00 -0700

    try calling gpt3

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import os
 from flask import Flask
+import openai
 
+from gpt import gpt3
 import secret
 
 app = Flask(__name__)
 
-OPENAI_KEY = os.environ.get(secret.OPENAI_KEY)
+openai.api_key = os.environ.get(secret.OPENAI_KEY)
 
 @app.route('/')
 def index():
-    if OPENAI_KEY:
-        return f'API Key found hehe'
-    else:
-        return 'API Key not found.'
+    return gpt3("There once was a ")
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ gpt.py
@@ -0,0 +1,36 @@
+import nltk
+import openai
+
+ENGINE = "gpt-3.5-turbo-instruct"
+MAX_NEW_TOKENS = 2048
+
+
+def gpt3(prompt: str) -> str:
+    max_tokens = int(
+        min(MAX_NEW_TOKENS, 4096-1.2*len(nltk.word_tokenize(prompt)))
+    )
+
+    if max_tokens < 10:
+        raise Exception("prompt was too long, lol.")
+
+    engine = "gpt-3.5-turbo-instruct"
+    temperature = 1.0
+    frequency_penalty = 2.0
+    presence_penalty = 2.0
+
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        n=1,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty,
+        # stop="##"
+    )
+    text = response.choices[0].text.strip()
+
+    text_preview = text[:25] if len(text) >= 25 else text
+    print(f"temperature: {temperature}\ntext: {text_preview}...")
+
+    return text
