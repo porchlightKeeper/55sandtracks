dadb9436417b82c1fae07896ccde09ce8fe99143
porchlightKeeper <porchlightkeeper@gmail.com>
2024-03-17 13:22:21 -0700

    nltk idk

--- gpt.py
+++ gpt.py
@@ -1,15 +1,5 @@
 import nltk
 import openai
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-nltk.download()
 
 ENGINE = "gpt-3.5-turbo-instruct"
 MAX_NEW_TOKENS = 2048
