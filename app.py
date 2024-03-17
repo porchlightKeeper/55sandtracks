f8deeec0e5b8168a63fa80996adee7f3c6016bfc
porchlightKeeper <porchlightkeeper@gmail.com>
2024-03-17 13:35:47 -0700

    try markdown

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 from flask import Flask
 import openai
+from flaskext.markdown import Markdown
 
 from gpt import gpt3
 import secret
 
-app = Flask(__name__)
-
 openai.api_key = os.environ.get(secret.OPENAI_KEY)
+
+app = Flask(__name__)
+Markdown(app)
+
 
 @app.route('/')
 def index():
-    return gpt3("There once was a ")
+    return "# 55sandtracks \n ## porchlight keeper \n\n *yes yes yes* \n \n" + gpt3("There once was a ")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
