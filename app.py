dd6dbf39c12dd8706874ad362176fbe3aa445d97
porchlightKeeper <porchlightkeeper@gmail.com>
2024-03-17 13:43:52 -0700

    markdown?

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template_string
 import openai
-from flaskext.markdown import Markdown
+import markdown
+
 
 from gpt import gpt3
 import secret
@@ -9,12 +10,20 @@ import secret
 openai.api_key = os.environ.get(secret.OPENAI_KEY)
 
 app = Flask(__name__)
-Markdown(app)
 
 
 @app.route('/')
 def index():
-    return "{% filter markdown %}\n# 55sandtracks \n## porchlight keeper \n\n *yes yes yes* \n \n" + gpt3("There once was a ")+"\n{% endfilter %}"
+    text = """
+    # 55sandtracks
+    ## porchlight keeper
+    *yes yes yes*
+    """
+
+    text += gpt3("There once was a ")
+
+    html_content = markdown.markdown(text)
+    return render_template_string('{{ content|safe }}', source=html_content)
 
 
 if __name__ == '__main__':
